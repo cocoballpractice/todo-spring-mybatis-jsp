6a270f3ad3f51ea2daecb26b86cd19b41ad4777e
mrcocoball <gunmaru93@gmail.com>
2022-11-24 23:47:14 +0900

    #07 - 컨트롤러 수정

--- 02/jdbctest/src/main/java/com/cocoball/jdbctest/controller/TodoListController.java
+++ 02/jdbctest/src/main/java/com/cocoball/jdbctest/controller/TodoListController.java
@@ -4,6 +4,7 @@ import com.cocoball.jdbctest.dto.TodoDTO;
 import com.cocoball.jdbctest.service.TodoService;
 import lombok.extern.log4j.Log4j2;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +19,7 @@ public class TodoListController extends HttpServlet {
 
     private TodoService todoService = TodoService.INSTANCE;
 
+    /*
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -32,6 +34,27 @@ public class TodoListController extends HttpServlet {
             throw new ServletException("list error!");
         }
 
+    }
+    */
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        log.info("todo list..................");
+
+        ServletContext servletContext = req.getServletContext();
+
+        log.info("appName:  "  + servletContext.getAttribute("appName"));
+
+
+        try {
+            List<TodoDTO> dtoList = todoService.listAll();
+            req.setAttribute("dtoList", dtoList);
+            req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req,resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("list error");
+        }
     }
 
 }
